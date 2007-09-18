9fa89755d8605bb645488a2234214a478a57ad61
joaquim.carvalho <joaquim.carvalho@cfdc76c7-8629-0410-aa24-fb5249ea4238>
2007-09-18 22:07:35 +0000

    cleaning up the iMac version

--- src/clio.bat
+++
@@ -1,6 +0,0 @@
-REM $Id: clio.bat,v 1.2 2006/05/16 10:52:42 Joaquim Exp $
-REM $Author: Joaquim $
-SET CLIOPATH=C:\users\joaquim\develop\rch\clio\src
-CD %CLIOPATH%
-"C:\Program files\pl\bin\plcon.exe" -f clioStart.pl -- -sf gacto.str -df %1 -echo yes
-
