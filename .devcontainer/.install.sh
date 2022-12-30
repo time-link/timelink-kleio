92fbbdd9b7d7ef905989cbb29acac9537372898a
Joaquim Carvalho <joaquim@uc.pt>
2022-12-30 13:00:37 +0000

    Script to install extra software

--- .devcontainer/.install.sh
+++ .devcontainer/.install.sh
@@ -1,1 +1,11 @@
 echo "Post creation install of necessary tools"
+echo
+echo "Installing swi Prolog"
+sudo apt-get update
+sudo apt-get install --assume-yes software-properties-common
+sudo apt-add-repository --yes ppa:swi-prolog/stable
+sudo apt-get update
+sudo apt-get install --assume-yes swi-prolog
+echo
+echo "Installing Postman cli tool Newman"
+npm install -g newman
