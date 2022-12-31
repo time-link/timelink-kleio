73d3d1d9d254ad8bda895d3b2ace623e8aefa7d9
Joaquim Carvalho <joaquim@uc.pt>
2022-12-31 01:37:52 +0000

    Add netcat to install script

--- .devcontainer/.install.sh
+++ .devcontainer/.install.sh
@@ -6,6 +6,8 @@ sudo apt-get install --assume-yes software-properties-common
 sudo apt-add-repository --yes ppa:swi-prolog/stable
 sudo apt-get update
 sudo apt-get install --assume-yes swi-prolog
+echo "Install nc"
+sudo apt-get install --assume-yes netcat
 echo
 echo "Installing Postman cli tool Newman"
 npm install -g newman
